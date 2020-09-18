d7cc4fe25fbef2692d1db3de1bb50ad944d82c7f
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-09-18 11:08:13 +0530

    defined routes

---
+++ src/routes.js
@@ -0,0 +1,5 @@
+export const FETCH_POPUP_MESSAGE =
+  '/getuser?url=insent-recruitment.web.app/&referralUrl=https://insent-recruitment.web.app/marketing-team';
+
+export const FETCH_ALL_MESSAGES =
+  '/user/channels/private-ZJDmvmyxbKVnwNAuQ16002355539191600236168480';
