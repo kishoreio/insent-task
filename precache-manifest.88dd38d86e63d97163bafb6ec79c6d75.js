eafafa50d08009ff1d23521d0fd62dc308c8537c
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-09-18 14:31:36 +0530

    Updates

---
+++ precache-manifest.88dd38d86e63d97163bafb6ec79c6d75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abeebc8d72ccf1f40dd4e606449d872",
+    "url": "/insent-task/index.html"
+  },
+  {
+    "revision": "9af8ba8677b175a53a9f",
+    "url": "/insent-task/static/css/main.46ac6e8c.chunk.css"
+  },
+  {
+    "revision": "244c488a940ad34789ef",
+    "url": "/insent-task/static/js/2.d76cdf7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insent-task/static/js/2.d76cdf7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af8ba8677b175a53a9f",
+    "url": "/insent-task/static/js/main.f380daeb.chunk.js"
+  },
+  {
+    "revision": "807a365146fce33096fe",
+    "url": "/insent-task/static/js/runtime-main.0ca76ca1.js"
+  },
+  {
+    "revision": "cccde26f4f6b0368eba3d3d2df1a862d",
+    "url": "/insent-task/static/media/logo.cccde26f.png"
+  }
+]);
